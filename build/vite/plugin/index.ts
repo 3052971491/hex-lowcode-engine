d1e5f8ceef554372fd5de80dd8a0128d4cae9ac5
yuanjunjie <3052971491@qq.com>
2023-04-20 18:00:14 +0800

    build: 新增打包分析

--- build/vite/plugin/index.ts
+++ build/vite/plugin/index.ts
@@ -2,9 +2,17 @@ import type { Plugin, PluginOption } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 import VueSetupExtend from 'vite-plugin-vue-setup-extend';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 export function createVitePlugins() {
   const vitePlugins: (Plugin | Plugin[] | PluginOption | PluginOption[])[] = [vue(), vueJsx(), VueSetupExtend()];
-
+  // 打包分析
+  vitePlugins.push(
+    visualizer({
+      open: true, // 注意这里要设置为true，否则无效
+      gzipSize: true,
+      brotliSize: true,
+    }),
+  );
   return vitePlugins;
 }
